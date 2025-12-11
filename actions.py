1afa46b930f6b0820d01c81e57a301309831fe64
Magp133 <ejrichardsward@gmail.com>
2025-12-11 18:30:19 +1300

    actions properly filled. step updated.

--- actions.py
+++ actions.py
@@ -1,82 +1,143 @@
 """
 File containing the different actions that can be taken.
 """
-
+import numpy as np
 import gymnasium as gym
 
 
-def search_for_drone_network(env: gym.Env) -> float:
+def search_for_drone_network(env) -> float:
     """
     Given the information within the state of the environment,
     determine if a network is found and update the state.
     """
+    # if the network has already been found then return small penalty.
+    if env.state["found_network"]:
+        return -0.1
+    
+    signal_strength = env.state["signal_strength"]
 
-    return 0.0
+    probability_of_success = (signal_strength + 100.0) / 100.0
 
-def join_drone_network(env: gym.Env) -> float:
+    if np.random.random() < probability_of_success:
+        env.state["found_network"] = True
+        return 1.0
+    else:
+        return -0.1
+
+def join_drone_network(env) -> float:
     """
     Given information within the state of the environment,
     attempt to join the drone network.
     """
 
-    return 0.0
+    if env.state["on_drone_network"]:
+        return -0.1
+    
+    signal_strength = env.state["signal_strength"]
 
-def capture_drone_wpa_pskey(env: gym.Env) -> float:
+    probability_of_success = (signal_strength + 100.0) / 100.0
+
+    if np.random.random() < probability_of_success:
+        env.state["on_drone_network"] = True
+        return 1.0
+    else:
+        return -0.1
+
+def capture_drone_wpa_pskey(env) -> float:
     """
     Given the information within the state of the environment,
     determine if sending a deauthenticaiton packet to client on the
     found network successfully captures the wpa_pskey. 
     """
 
-    return 0.0
+    # check the prerequesites are met.
+    if not env.state["found_network"] and not env.state["has_wpa_pskey"]:
+        return -0.5
 
-def crack_drone_password(env: gym.Env) -> float:
+    # does the drone network have a password?
+    if not env.state["network_has_password"]:
+        return -0.1
+    
+    if np.random.random() < 0.5:
+        env.state["has_wpa_pskey"] = True
+        return 1.0
+    else:
+        return -0.1
+
+def crack_drone_password(env) -> float:
     """
     Given the information within the state of the environment,
     start the process of cracking the password.
     """
 
-    return 0.0
+    if not env.state["has_wpa_key"]:
+        return -0.5
+    else:
+        env.state["password_cracking_started"] = True
+        return 1.0
 
-def flood_drone_port(env: gym.Env) -> float:
+
+def flood_drone_port(env) -> float:
     """
     Given the information within the state of the environment,
     flood the communication port of the drone.
     Must be on the network.
     """
+    if env.state["drone_status"] != 0 and env.state["on_drone_network"]: # drone is controlled, crashed or already dos
+        return -0.1
+    
+    if env.state["target_port_vulnerable"]:
+        env.state["drone_status"] = 2 # the drone has been crashed.
+        return 1.0
+    else:
+        return -0.1
 
-    return 0.0
-
-def change_drone_network_password(env: gym.Env) -> float:
+def change_drone_network_password(env) -> float:
     """
     Given the information within the state of the environment,
     attempt to change the network password.
     """
 
-    return 0.0
+    if env.state["on_drone_network"]:
+        if np.random.random() < 0.5:
+            env.state["drone_status"] = 1 # drone is now controlled
+            return 5.0
 
-def land_drone_func(env: gym.Env) -> float:
+    return -0.1
+
+def land_drone_func(env) -> float:
     """
     Given the information within the state of the environment,
     attempt to land the drone through injection.
     (simulated)
     """
 
-    return 0.0
+    if env.state["on_drone_network"] and env.state["drone_status"] == 1: # agent is on drone network and drone is controlled.
+        # trying to land doesnt always work.
+        if np.random.random() < 0.5:
+            env.status["drone_status"] = 3 # drone is landed.
+            return 10.0
+        else:
+            return 0.1
+    else:
+        return -0.1
 
-def crash_drone_func(env: gym.Env) -> float:
+def crash_drone_func(env) -> float:
     """
     Given the information within the state of the environment,
     attempt to crash the drone through injection.
     """
+    if env.state["on_drone_network"] and env.state["drone_status"] == 1:
+        env.status["drone_status"] = 2 # drone is crashed.
+        return 5.0
+    return -0.1
 
-    return 0.0
-
-def jam_drone_signals(env: gym.Env) -> float:
+def jam_drone_signals(env) -> float:
     """
     Given the information within the state of the environment,
     simulate jamming all signals in the area. 
     massive penalty. does not require being on the network. 
     """
+    env.state["drone_status"] = 2
 
-    return 0.0
+    return -10.0
--- core_simulator.py
+++ core_simulator.py
@@ -17,24 +17,14 @@ class drone_env(gym.Env):
 
     Check cyborg default wrapper. 
     """
-    def __init__(self):
-        """
-        state array:
-        - network found: 0 | 1
-        - drone status: (0, operational), (1, DOS), (2, crashed), (3, controlled)
-        - signal strength: -100 -> 0
-        - crack progress: 0.0 -> 1.0 (0% to 100%)
-        - time since success: 0.0 -> inf (time since a meaningful action was successful)
-        """
-                
+    def __init__(self):    
         # observation space
         self.observation_space = Dict({
             "network_found" : Discrete(2), # network found, True 1, False 0
             "joined_network" : Discrete(2), # is the agent on the network?
             "drone_status" : Discrete(4), # 0: operational, 1: DOS, 2: crashed, 3: controlled
             "signal_strength_dbm" : Box(low=-100.0, high=0.0, shape=(), dtype=np.float32), # RSSI -100 => 0
-            "cracking_progress" : Box(low=0.0, high=1.0, shape=(), dtype=np.float32), # 0.0 => 1.0
-            "time_since_success" : Box(low=0.0, high=np.inf) # time since a meaningful action
+            "cracking_progress" : Box(low=0.0, high=1.0, shape=(), dtype=np.float32) # 0.0 => 1.0
             })
 
         # action space
@@ -51,12 +41,13 @@ class drone_env(gym.Env):
         })
 
         self.state = {
-            "drone_status" : "operational", # operational, controlled, crashed
+            "drone_status" : 0, # 0, operational, 1 controlled, 2 crashed, 3 landed
             "found_network" : False, # has the agent identified a drone network. 
             "on_drone_network" : False, # is the agent connected to the drone's network.
             "network_has_password" : self.np_random.choice([True, False]), # determine if the drone network is password protected. 
             "password_list_has_password" : self.np_random.choice([True, False]), # determine if the drone's network password is contained by the password list.
-            "signal_strength" : self.np_random.uniform(low= -100.0, high= -85.0), # signal strength of the drone. Starts far away. 
+            "signal_strength" : -100, # signal strength of the drone. Starts far away. 
+            "password_cracking_started" : False, # has the password cracking began
             "password_cracking_progress" : 0.0, # cracking progress of the password
             "has_wpa_pskey" : False, # has the agent found the wpa pskey.
             "last_action" : None, # stores the last action
@@ -74,23 +65,26 @@ class drone_env(gym.Env):
         """
         Reset the environment to initial states.
         """
-        init_network_status = 0 # not found
-        init_drone_status = 0 # operational
-        init_rssi = self.np_random.uniform(low=-100.0, high=-85.0) # low signal strength. Drone is far away. 
-        init_crack_progress = 0.0 # 0% progress.
-        init_time_since_success = 0.0 # start of episode
+        self.state["drone_status"] = "operational"
+        self.state["found_network"] = False
+        self.state["on_drone_network"] = False
+        
+        self.state["network_has_password"] = self.np_random.choice([True, False]) 
+        self.state["password_list_has_password"] = self.np_random.choice([True, False]) 
+        self.state["signal_strength"] = self.np_random.uniform(low= -100.0, high= -85.0) 
+        
+        self.state["password_cracking_progress"] = 0.0
+        self.state["has_wpa_pskey"] = False
+        self.state["password_cracking_started"] = False
+        self.state["target_port_vulnerable"] = self.np_random.choice([True, False])
+        
 
-        self.current_obs = np.array([
-            init_network_status,
-            init_drone_status,
-            init_rssi,
-            init_crack_progress,
-            init_time_since_success
-        ], dtype=np.float32)
 
         self.current_step = 0
         info = {}
-        return self.current_obs, info
+        observation = self._obs()
+
+        return observation, info
 
     def step(self, action: dict):
         """
@@ -100,6 +94,7 @@ class drone_env(gym.Env):
         Need to change to disallow multiple actions taken during a single step. 
         """
         
+
         reward =0.05
         terminated = False
         truncated = False
@@ -116,50 +111,63 @@ class drone_env(gym.Env):
         crash_drone = action["crash_drone"]
         jam_signals = action["jam_signals"]
 
-        if search_networks == 1:
+        # ensure that there is only 1 action that can be taken.
+
+        active_actions = sum(action.values())
+
+        if active_actions > 1:
+            reward = -10
+            truncated = True
+            info["error"] = "Tried doing multiple actions at the same time."
+
+        elif search_networks == 1:
             func_reward = search_for_drone_network(self)
             reward += func_reward
 
-        if join_network == 1:
+        elif join_network == 1:
             func_reward = join_drone_network(self)
             reward += func_reward
 
-        if capture_wpa_pskey == 1:
+        elif capture_wpa_pskey == 1:
             func_reward = capture_drone_wpa_pskey(self)
             reward += func_reward
         
-        if crack_password == 1:
+        elif crack_password == 1:
             func_reward = crack_drone_password(self)
             reward += func_reward
         
-        if flood_port == 1:
+        elif flood_port == 1:
             func_reward = flood_drone_port(self)
             reward += func_reward
 
-        if change_network_password == 1:
+        elif change_network_password == 1:
             func_reward = change_drone_network_password(self)
             reward += func_reward
 
-        if land_drone == 1:
+        elif land_drone == 1:
             func_reward = land_drone_func(self)
             reward += func_reward
 
-        if crash_drone == 1:
+        elif crash_drone == 1:
             func_reward = crash_drone_func(self)
             reward += func_reward
         
-        if jam_signals == 1:
+        elif jam_signals == 1:
             func_reward = jam_drone_signals(self)
             reward += func_reward
         
 
-        # get the observation
+        # get the observation and update the overall internal state. 
         observation = self._obs()
+        self.update_state()
 
         self.current_step += 1
 
         if self.current_step >= 500:
             truncated = True
+
+        if self.state["drone_status"] == 2 or self.state["drone_status"] == 3: # drone has crashed or landed
+            terminated = True
 
         return observation, reward, terminated, truncated, info
 
@@ -167,7 +175,16 @@ class drone_env(gym.Env):
         """
         Returns the observation of the environment. Uses self.state to determine the current observation.
         """
-        pass
+
+        self.current_obs = np.array([
+            self.state["found_network"],
+            self.state["on_drone_network"],
+            self.state["drone_status"],
+            self.state["signal_strength"],
+            self.state["password_cracking_progress"],
+        ], dtype=np.float32)
+        
+        return self.current_obs
 
     def update_state(self):
         pass
